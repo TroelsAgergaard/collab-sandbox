48896811d14645699e8913252acff9ebfc3014ad
Troels Agergaard <tras@kea.dk>
2024-09-04 17:31:37 +0200

    Add dynamic class to enhance heading

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.querySelector("h1, title").textContent = "Welcome...";
 document.querySelector(".welcome").textContent =
   "This is new - Welcome to My Website";
+document.querySelector("h1").classList.add("xxlarge");
